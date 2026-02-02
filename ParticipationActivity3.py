469ea1e943c21dcd2de07b92a85507b7978e6159
Darian Bruce <darianmariebruce@gmail.com>
2026-02-01 22:44:32 -0500

    randomizer and print statement set up

--- ParticipationActivity3.py
+++ ParticipationActivity3.py
@@ -12,3 +12,9 @@ descriptive_words = ['cutie', 'bubbly', 'sweet', 'little', 'snuggle', 'honey', '
 nouns = ['pie', 'pudding', 'muffin', 'cupcake', 'dumpling', 'bear', 'buttercup',
          'bean', 'bug', 'sprout', 'cake', 'face', 'button', 'doll', 'kitten',
          'berry', '']
+
+descriptive_word_choice = random.choice(descriptive_words).title()
+
+noun_choice = random.choice(nouns).title()
+
+print(f"Your generated cute name is: {descriptive_word_choice} {noun_choice}")

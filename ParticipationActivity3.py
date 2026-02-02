67641032fadfa02a0bb7f304f88db37d86baa9d2
Darian Bruce <darianmariebruce@gmail.com>
2026-02-01 23:10:39 -0500

    finished randomizer

--- ParticipationActivity3.py
+++ ParticipationActivity3.py
@@ -1,5 +1,6 @@
 '''Participation Activity 3
 Darian Marie Bruce
+cute name generator
 02/01/2026'''
 
 import random
@@ -11,10 +12,207 @@ descriptive_words = ['cutie', 'bubbly', 'sweet', 'little', 'snuggle', 'honey', '
                      'milky', 'delightful', 'dreamy', 'starry', 'sweet']
 nouns = ['pie', 'pudding', 'muffin', 'cupcake', 'dumpling', 'bear', 'buttercup',
          'bean', 'bug', 'sprout', 'cake', 'face', 'button', 'doll', 'kitten',
-         'berry', '']
+         'berry', 'peach', 'turkey', 'lamb', 'panda', 'biscuit', 'chick', 'gumdrop', 'kitty', 'creampuff', 'cup', 'nut', 'peanut'
+         'waffle', 'cookie', 'moonpie', 'doodle', 'biscuit']
 
-descriptive_word_choice = random.choice(descriptive_words).title()
+# generator logic and text
 
-noun_choice = random.choice(nouns).title()
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
 
-print(f"Your generated cute name is: {descriptive_word_choice} {noun_choice}")
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+# 32 more results until all list items are used
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
+
+descriptive_word_choice = (descriptive_words.pop(random.randint(0, len(descriptive_words)-1))).title()
+
+noun_choice = (nouns.pop(random.randint(0, len(nouns)-1))).title()
+
+print(f"Your generated cute name is:\n\t{descriptive_word_choice} {noun_choice}")
